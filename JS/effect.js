2522773f8cae07e93db367b6b756509df63f2f4f
iallawi50 <aahh50018@gmail.com>
2021-07-03 21:19:53 +0300

    add effect

--- JS/effect.js
+++ JS/effect.js
@@ -1,8 +1,35 @@
-let header = document.querySelector(".up");
+let school = document.querySelector(".effect-right-school");
+
+
+let skills1 = document.querySelector(".effect-right-skills1");
+let skills2 = document.querySelector(".effect-right-skills2");
+let skills3 = document.querySelector(".effect-bottom-skills3");
+let skills4 = document.querySelector(".effect-bottom-skills4");
+let skills5 = document.querySelector(".effect-left-skills5");
+let skills6 = document.querySelector(".effect-left-skills6");
+
+let brand1 = document.querySelector(".effect-right-brand");
+let brand2 = document.querySelector(".effect-left-brand");
+
+let span = document.querySelector(".up");
+
+
+
 
 window.onscroll = function () {
-    this.scrollY >= 200 ? span.classList.add("show") : span.classList.remove("show");
+    this.scrollY >= 250 ? span.classList.add("show") : span.classList.remove("show");
+
+    this.scrollY >= 3 ? school.classList.add("effect-right-school-show") : '';
+    this.scrollY >= 300 ? skills1.classList.add("effect-right-skills-show") : '';
+    this.scrollY >= 300 ? skills2.classList.add("effect-right-skills-show") : '';
+    this.scrollY >= 300 ? skills3.classList.add("effect-bottom-skills-show") : '';
+    this.scrollY >= 300 ? skills4.classList.add("effect-bottom-skills-show") : '';
+    this.scrollY >= 300 ? skills5.classList.add("effect-left-skills-show") : '';
+    this.scrollY >= 300 ? skills6.classList.add("effect-left-skills-show") : '';
+    this.scrollY >= 400 ? brand1.classList.add("effect-right-brand-show") : '';
+    this.scrollY >= 400 ? brand2.classList.add("effect-left-brand-show") : '';
 };
+
 
 span.onclick = function () {
     window.scrollTo({
