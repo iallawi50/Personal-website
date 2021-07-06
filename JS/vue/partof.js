0016575545a6665cd00c0663a269990cb941e6f3
iallawi50 <aahh50018@gmail.com>
2021-07-06 14:00:47 +0300

    changing the font and create 'partof' Component by Vuejs

---
+++ JS/vue/partof.js
@@ -0,0 +1,58 @@
+Vue.component("partof", {
+    template: `<div>
+
+
+
+<div class="row">
+
+
+<div class="col-lg-6 col-md-6 col-sm-12" v-for="item in items">
+<a href="https://cast-media.netlify.app" target="_blank">
+    <div class="card text-dark bg-light mb-3" :class="item.class" style="max-width: 30rem;">
+        <div class="card-body">
+            <p class="card-text text-center">
+                <img :src="item.img" :width="item.size" alt="">
+            </p>
+
+
+            <center>
+
+                <small class="text-muted">{{ item.role }}</small>
+            </center>
+
+        </div>
+        <div class="card-footer text-white text-center bg-custom">{{ item.title }}</div>
+    </div>
+</a>
+</div>
+
+
+
+    </div>
+                        
+    </div>
+    `,
+    data() {
+        return {
+
+            items: [
+                {
+                    img: "./partof/castmedia.png",
+                    title: "CastMedia",
+                    class: "effect-right-brand",
+                    role: "مؤسس",
+                    size: "200px"
+                },
+                {
+                    img: "./partof/bitcode.png",
+                    title: "BitCode",
+                    class: "effect-left-brand",
+                    role: "متدرب",
+                    size: "112px"
+                },
+            ],
+        }
+    },
+
+
+})
